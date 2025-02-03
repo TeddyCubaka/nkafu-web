1c6434984c6c45eca69da2f4c2bce5c24178661e
Teddy Cubaka <teddycubaka96@gmail.com>
2025-02-03 07:49:43 +0100

    update input boolean

--- src/components/form/inputs/booleanInput.tsx
+++ src/components/form/inputs/booleanInput.tsx
@@ -1,5 +1,5 @@
-// src/components/Input/BooleanInput.tsx
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
 import { InputType } from "./types";
 
 const BooleanInput: React.FC<InputType> = ({
@@ -9,20 +9,38 @@ const BooleanInput: React.FC<InputType> = ({
   isOptional,
   property
 }) => {
+  useEffect(() => {
+    if (value?.value === undefined) {
+      setValue?.({ ...value, value: false });
+    }
+  }, []);
+
   const handleChange = () => {
     setValue?.({ ...value, value: !value?.value });
   };
 
   return (
-    <input
-      type="checkbox"
-      name={property}
-      id={id}
-      checked={!!value?.value}
-      onChange={handleChange}
-      required={!isOptional}
-      className="h-5 w-5 rounded-full border-gray-300 bg-bg-secondary text-foreground focus:border-background focus:ring-primary"
-    />
+    <button
+      type="button"
+      role="switch"
+      aria-checked={!!value?.value}
+      onClick={handleChange}
+      className={`
+        relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full 
+        border-2 border-transparent transition-colors duration-200 ease-in-out 
+        focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2
+        ${value?.value ? 'bg-primary' : 'bg-gray-200'}
+      `}
+    >
+      <span className="sr-only">{value?.value ? 'On' : 'Off'}</span>
+      <span
+        className={`
+          pointer-events-none inline-block h-5 w-5 transform rounded-full 
+          bg-white shadow ring-0 transition duration-200 ease-in-out
+          ${value?.value ? 'translate-x-5' : 'translate-x-0'}
+        `}
+      />
+    </button>
   );
 };
 
